5608d6507d830f8e43311ddf6516c2fa6708bbee
ITNicekot <kotnice5@gmail.com>
2026-03-09 09:16:50 +0100

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AlertsModule } from './modules/alerts/alerts.module';
-import { UploadModule } from './modules/upload/upload.module';
+/*import { UploadModule } from './modules/upload/upload.module';*/
 import { AuthModule } from './modules/auth/auth.module';
 import { LikesModule } from './modules/likes/likes.module';
 import { CommentsModule } from './modules/comments/comments.module';
